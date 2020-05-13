d76796007d125ce91e0042d12c2917fab121e10f
Jonathan Orta <emanuelorta93@hotmail.com>
2020-05-13 18:17:25 -0500

    All functions added

--- src/Economics.php
+++ src/Economics.php
@@ -2,34 +2,116 @@
 
 use Exception;
 
-/**
-*  A sample class
-*
-*  Use this section to define what this class is doing, the PHPDocumentator will use this
-*  to automatically generate an API documentation using this information.
-*
-*  @author yourname
-*/
 class Economics {
 
-   /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-   protected $present = 0;
-   protected $future = 0;
-   protected $interest = 0;
-   protected $periods = 0;
+    protected $present;
+    protected $future;
+    protected $interest;
+    protected $periods;
+
+    public function __construct($present = 0, $interest = 0, $periods = 0, $future = 0)
+    {
+        $this->present = $present;
+        $this->interest = $interest;
+        $this->periods = $periods;
+        $this->future = $future;
+    }
 
     /**
      * @param null $present
+     * @return int|Economics
      * @throws Exception
      */
     public function present($present = null)
-   {
-       if (!is_null($present)) {
-           if (is_numeric($present)) {
-               $this->present = $present;
-           } else {
-               throw new Exception('Present value has to be a numeric value');
-           }
-       }
-   }
+    {
+        if (!is_null($present)) {
+            if (is_numeric($present)) {
+                $this->present = $present;
+                return $this;
+            } else {
+                throw new Exception('Present value has to be a numeric value');
+            }
+        } else {
+            if ($this->future > 0 && $this->interest > 0 && $this->periods > 0) {
+                return 1;
+            } else {
+                throw new Exception('Impossible to get present value without all parameters');
+            }
+        }
+    }
+
+    /**
+     * @param null $future
+     * @return $this|int
+     * @throws Exception
+     */
+    public function future($future = null)
+    {
+        if (!is_null($future)) {
+            if (is_numeric($future)) {
+                $this->future = $future;
+                return $this;
+            } else {
+                throw new Exception('Future value has to be a numeric value');
+            }
+        } else {
+            if ($this->present > 0 && $this->interest > 0 && $this->periods > 0) {
+                return 8;
+            } else {
+                throw new Exception('Impossible to get future value without all parameters');
+            }
+        }
+    }
+
+    /**
+     * @param null $interest
+     * @return $this|int
+     * @throws Exception
+     */
+    public function interest($interest = null)
+    {
+        if (!is_null($interest)) {
+            if (is_numeric($interest)) {
+                if ($interest > 1) {
+                    if ($interest > 100) {
+                        $interest = $interest - 100;
+                    }
+                    $interest = $interest/100;
+                }
+                $this->interest = $interest;
+                return $this;
+            } else {
+                throw new Exception('Interest value has to be a numeric value');
+            }
+        } else {
+            if ($this->present > 0 && $this->future > 0 && $this->periods > 0) {
+                return 5;
+            } else {
+                throw new Exception('Impossible to get interest value without all parameters');
+            }
+        }
+    }
+
+    /**
+     * @param null $periods
+     * @return $this|int
+     * @throws Exception
+     */
+    public function periods($periods = null)
+    {
+        if (!is_null($periods)) {
+            if (is_numeric($periods)) {
+                $this->periods = $periods;
+                return $this;
+            } else {
+                throw new Exception('Period value has to be a numeric value');
+            }
+        } else {
+            if ($this->present > 0 && $this->future > 0 && $this->interest > 0) {
+                return 9;
+            } else {
+                throw new Exception('Impossible to get periods value without all parameters');
+            }
+        }
+    }
 }
